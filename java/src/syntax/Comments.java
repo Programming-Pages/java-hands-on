6424286d2d09800db34c335186aeefafb1404adc
deepthyjayaraj <deepthyjayaraj@gmail.com>
2021-05-08 13:34:39 -0700

    Java Codes

---
+++ java/src/syntax/Comments.java
@@ -0,0 +1,17 @@
+package syntax;
+
+public class Comments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JAVA");
+		
+		// this is a comment
+		
+		/*
+		 * This is a multi-line comment
+		 */
+
+	}
+
+}
---
+++ java/src/syntax/VString.java
@@ -0,0 +1,12 @@
+package syntax;
+
+public class VString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "Java";
+		System.out.println(name);
+	}
+
+}
